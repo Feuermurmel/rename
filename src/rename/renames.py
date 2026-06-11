afd44942bb2dc94bf6956fa7602fb0dd2384b3b2
Michi Schwarz <michi.schwarz@gmail.com>
2026-06-11 11:33:32 +0200

    Remove directories that get empty while renaming

--- src/rename/renames.py
+++ src/rename/renames.py
@@ -1,4 +1,5 @@
 import logging
+import shutil
 from collections import Counter
 from functools import reduce
 from pathlib import Path
@@ -7,6 +8,8 @@ from typing import Never
 from rename.renames_file import Rename
 from rename.renames_file import Renames
 from rename.utils import UserError
+
+_ignored_files_when_cleansing_directories = [".DS_Store"]
 
 
 def _path_to_str(path: Path) -> str:
@@ -77,23 +80,55 @@ def create_renames(file_paths: list[Path], explicit_base_path: Path | None) -> R
 
 def apply_renames(renames: Renames) -> None:
     moved_files = []
+    already_renamed_old_paths = set()
+    dirs_to_cleanse: Counter[Path] = Counter()
+
+    def add_dir_to_cleanse(base_path: Path, dir: Path) -> None:
+        if base_path in dir.parents:
+            if dir not in dirs_to_cleanse:
+                add_dir_to_cleanse(base_path, dir.parent)
+
+            dirs_to_cleanse[dir] += 1
+
+    def cleanse_dir(dir: Path) -> None:
+        if dirs_to_cleanse[dir]:
+            dirs_to_cleanse[dir] -= 1
+
+            if not dirs_to_cleanse[dir]:
+                if all(
+                    i.name in _ignored_files_when_cleansing_directories
+                    and i.is_file(follow_symlinks=False)
+                    for i in dir.iterdir()
+                ):
+                    logging.info(f"Removing directory {dir}.")
+                    shutil.rmtree(dir)
+                    cleanse_dir(dir.parent)
 
     for rename in renames.renames:
-        old_dir = Path(rename.base_path) / rename.old_dir
-        old_path = old_dir / f"{rename.old_name}{rename.old_suffix}"
-        new_dir = Path(rename.base_path) / rename.new_dir
-        new_path = new_dir / f"{rename.new_name}{rename.new_suffix}"
+        base_path = Path(rename.base_path)
+        old_path = base_path / rename.old_dir / f"{rename.old_name}{rename.old_suffix}"
+        new_path = base_path / rename.new_dir / f"{rename.new_name}{rename.new_suffix}"
 
         if old_path.exists(follow_symlinks=False):
-            if old_path != new_path:
-                if new_path.exists(follow_symlinks=False) and not old_path.samefile(
-                    new_path
-                ):
+            if old_path == new_path:
+                continue
+            elif new_path.exists(follow_symlinks=False):
+                if not old_path.samefile(new_path):
                     raise UserError(f"Destination {new_path} already exists.")
-
-                moved_files.append((old_path, new_path))
         elif not new_path.exists(follow_symlinks=False):
             raise UserError(f"File {old_path} does not exist.")
+        else:
+            already_renamed_old_paths.add(old_path)
+
+        moved_files.append((old_path, new_path))
+        add_dir_to_cleanse(base_path, old_path.parent)
+
+    for old_path, new_path in moved_files:
+        if not old_path.exists(follow_symlinks=False):
+            if not new_path.exists(follow_symlinks=False):
+                raise UserError(f"File {old_path} does not exist.")
+
+            already_renamed_old_paths.add(old_path)
 
     old_paths = Counter(i for i, _ in moved_files)
     new_paths = Counter(i for _, i in moved_files)
@@ -109,14 +144,20 @@ def apply_renames(renames: Renames) -> None:
     for i in set(old_paths).intersection(new_paths):
         raise UserError(f"{i} is both a source and a destination.")
 
-    if not moved_files:
-        logging.info("No files to rename.")
+    did_rename = False
 
     for old_path, new_path in moved_files:
-        if not new_path.parent.exists(follow_symlinks=True):
-            logging.info(f"Creating directory at {new_path.parent}.")
-            new_path.parent.mkdir(parents=True)
+        if old_path not in already_renamed_old_paths:
+            if not new_path.parent.exists(follow_symlinks=True):
+                logging.info(f"Creating directory at {new_path.parent}.")
+                new_path.parent.mkdir(parents=True)
 
-        logging.info(f"Renaming {old_path} to {new_path}.")
+            logging.info(f"Renaming {old_path} to {new_path}.")
 
-        old_path.replace(new_path)
+            old_path.rename(new_path)
+            did_rename = True
+
+        cleanse_dir(old_path.parent)
+
+    if not did_rename:
+        logging.info("No files to rename.")
